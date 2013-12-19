1e5840072082d0ffbd6e41af3f45219533355c5b
Steven Rogers <stevenr41@hotmail.com>
2013-12-19 11:27:40 -0800

    Last chance to get this working!

--- brikout_v1/settings.py
+++ brikout_v1/settings.py
@@ -64,12 +64,19 @@ WSGI_APPLICATION = 'brikout_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+
+
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+
 
 import os
 
