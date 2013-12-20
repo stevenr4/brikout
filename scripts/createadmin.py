ae168637f74f7c42eec987efbf8d0ae9c770dd3a
Steven Rogers <stevenr41@hotmail.com>
2013-12-20 11:18:44 -0800

    "another fix to createadmin"

--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -3,16 +3,16 @@
 print "Running scripts/createadmin.py"
 
 from django.contrib.auth.models import User
-from app.models import UserExtension
+#from app.models import UserExtension
 
 if User.objects.count() == 0:
-    print "Creating an admin..."
-    admin = User.objects.create(username='stevenr4')
-    admin.set_password('tmppass')
-    admin.is_superuser = True
-    admin.is_staff = True
-    admin.save()
-    admin_ext = UserExtension(user = admin)
-    admin_ext.save()
-    print "Success creating admin"
+	print "Creating an admin..."
+	admin = User.objects.create(username='stevenr4')
+	admin.set_password('tmppass')
+	admin.is_superuser = True
+	admin.is_staff = True
+	admin.save()
+	# admin_ext = UserExtension(user = admin)
+	# admin_ext.save()
+	print "Success creating admin"
 
